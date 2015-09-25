e69448913638f54b3cfbad04c2200fdd449649b3
walid <walid.sa3d@gmail.com>
2015-09-25 13:18:58 +0100

    change project structure

---
+++ browseragents/main.py
@@ -0,0 +1,18 @@
+import useragentdata as data
+
+from random import choice
+
+
+def random_useragent(browser=None, os=None):
+    agents = data.agents
+    if browser and os:
+        agents = [x for x in agents if browser.lower(
+        ) in x['browserName'].lower() if os.lower() in x['osName'].lower()]
+    elif browser:
+        agents = [
+            x for x in agents if browser.lower() in x['browserName'].lower()]
+    elif os:
+        agents = [x for x in agents if os.lower() in x['osName'].lower()]
+    return choice(agents)['userAgent']
+
+print random_useragent()
